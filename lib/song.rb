9a876dabcbda7ad6868d6f99fa0fa32251f16cbe
Daniel Kwon <danielkwon89@gmail.com>
2020-05-19 17:56:10 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+class Song
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  attr_accessor :name, :artist, :genre
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    genres_hash = {}
+    self.genres.each {|genre| genres_hash[genre] = 0}
+    @@genres.each {|genre| genres_hash[genre] += 1}
+    genres_hash
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.artist_count
+    artists_hash = {}
+    self.artists.each {|artist| artists_hash[artist] = 0}
+    @@artists.each {|artist| artists_hash[artist] += 1}
+    artists_hash
+  end
+  
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@genres.push(genre)
+    @@artists.push(artist)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+end
